328864b1847c5a318e6aa8824730bfc18c048169
Rob B <rob@rjrobbie.dev>
2024-03-19 21:09:45 +0000

    iterated over array with for of loop and rendered in browser, and used import export to fetch data

--- script.js
+++ script.js
@@ -1,181 +1,23 @@
-const catsData = [
-  {
-    emotionTags: ["moody"],
-    isGif: false,
-    image: "angry.jpeg",
-    alt: "A cat looking moody",
-  },
-  {
-    emotionTags: ["moody", "insomniac"],
-    isGif: false,
-    image: "angry2.jpeg",
-    alt: "A cat looking moody",
-  },
-  {
-    emotionTags: ["moody"],
-    isGif: false,
-    image: "angry3.jpeg",
-    alt: "A cat looking moody",
-  },
-  {
-    emotionTags: ["confused", "sad"],
-    isGif: false,
-    image: "confused.jpeg",
-    alt: "A cat looking confused",
-  },
-  {
-    emotionTags: ["dominant", "moody"],
-    isGif: false,
-    image: "dominant.jpeg",
-    alt: "A cat looking dominant",
-  },
-  {
-    emotionTags: ["happy", "relaxed"],
-    isGif: false,
-    image: "happy.jpeg",
-    alt: "A cat looking happy",
-  },
-  {
-    emotionTags: ["hungry"],
-    isGif: false,
-    image: "hungry.jpeg",
-    alt: "A cat looking hungry",
-  },
-  {
-    emotionTags: ["hungry"],
-    isGif: false,
-    image: "hungry1.jpeg",
-    alt: "A cat looking hungry",
-  },
-  {
-    emotionTags: ["insomniac"],
-    isGif: false,
-    image: "insomnia.jpeg",
-    alt: "A cat looking insomniac",
-  },
-  {
-    emotionTags: ["insomniac"],
-    isGif: false,
-    image: "insomnia1.jpeg",
-    alt: "A cat looking insomniac",
-  },
-  {
-    emotionTags: ["relaxed"],
-    isGif: false,
-    image: "lazy.jpeg",
-    alt: "A cat looking lazy",
-  },
-  {
-    emotionTags: ["scared"],
-    isGif: false,
-    image: "nervous.jpeg",
-    alt: "A cat looking nervous",
-  },
-  {
-    emotionTags: ["sad"],
-    isGif: false,
-    image: "sad.jpeg",
-    alt: "A cat looking sad",
-  },
-  {
-    emotionTags: ["sad", "moody"],
-    isGif: false,
-    image: "sad1.jpeg",
-    alt: "A cat looking sad",
-  },
-  {
-    emotionTags: ["moody"],
-    isGif: true,
-    image: "angry.gif",
-    alt: "A cat looking moody",
-  },
-  {
-    emotionTags: ["moody"],
-    isGif: true,
-    image: "angry2.gif",
-    alt: "A cat looking angry",
-  },
-  {
-    emotionTags: ["confused"],
-    isGif: true,
-    image: "confused2.gif",
-    alt: "A cat looking confused",
-  },
-  {
-    emotionTags: ["dominant"],
-    isGif: true,
-    image: "dominant.gif",
-    alt: "A cat looking dominant",
-  },
-  {
-    emotionTags: ["happy"],
-    isGif: true,
-    image: "happy.gif",
-    alt: "A cat looking happy",
-  },
-  {
-    emotionTags: ["hungry", "sad", "confused"],
-    isGif: true,
-    image: "confused.gif",
-    alt: "A cat looking hungry",
-  },
-  {
-    emotionTags: ["hungry"],
-    isGif: true,
-    image: "hungry.gif",
-    alt: "A cat looking hungry",
-  },
-  {
-    emotionTags: ["hungry"],
-    isGif: true,
-    image: "hungry2.gif",
-    alt: "A cat looking hungry",
-  },
-  {
-    emotionTags: ["insomniac", "scared"],
-    isGif: true,
-    image: "insomnia2.gif",
-    alt: "A cat looking insomniac",
-  },
-  {
-    emotionTags: ["relaxed"],
-    isGif: true,
-    image: "lazy.gif",
-    alt: "A cat looking relaxed",
-  },
-  {
-    emotionTags: ["relaxed"],
-    isGif: true,
-    image: "relaxed2.gif",
-    alt: "A cat looking relaxed",
-  },
-  {
-    emotionTags: ["scared", "sad"],
-    isGif: true,
-    image: "nervous.gif",
-    alt: "A cat looking nervous",
-  },
-  {
-    emotionTags: ["scared"],
-    isGif: true,
-    image: "nervous2.gif",
-    alt: "A cat looking scared",
-  },
-  {
-    emotionTags: ["sad"],
-    isGif: true,
-    image: "sad.gif",
-    alt: "A cat looking sad",
-  },
-];
+import { catsData } from "./data.js";
+
+const emotionRadios = document.getElementById("emotion-radios");
 
 function getEmotionsArray(cats) {
-
-    const newArray = []
+  const newArray = [];
 
   for (let cat of catsData) {
-      for (let emotion of cat.emotionTags)
-      newArray.push(emotion)
+    for (let emotion of cat.emotionTags) newArray.push(emotion);
   }
-      return newArray
+  return newArray;
 }
+
+function renderEmotionsRadios(cats) {
+  const emotions = getEmotionsArray(cats);
+  let string = "";
+  for (let emotion of emotions) {
+    string += `<p>${emotion}</p>`;
+  }
+  emotionRadios.innerHTML = string;
+}
+
+renderEmotionsRadios(catsData);
